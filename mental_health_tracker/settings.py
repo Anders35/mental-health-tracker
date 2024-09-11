73e8b6f1397099d528b81da05f68004558a7e9d5
Anders35 <andersleo05@gmail.com>
2024-09-11 13:06:18 +0700

    fix CSRF_TRUSTED_ORIGINS

--- mental_health_tracker/settings.py
+++ mental_health_tracker/settings.py
@@ -123,4 +123,4 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CSRF_TRUSTED_ORIGINS = ["http://localhost","http://127.0.0.1","http://anders-willard-mentalhealthproject.pbp.cs.ui.ac.id", "https://anders-willard-mentalhealthproject.pbp.cs.ui.ac.id"]
+CSRF_TRUSTED_ORIGINS = ["http://localhost","http://127.0.0.1","http://anders-willard-mentalhealthtracker.pbp.cs.ui.ac.id", "https://anders-willard-mentalhealthtracker.pbp.cs.ui.ac.id"]
